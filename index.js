2a5b49f3a04b81e4a82c4e5d28a7b114cb24baee
Julie Jonak <juliejonak10@gmail.com>
2019-01-15 21:42:37 -0600

    Lecture code completed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
-const db = require('./database/dbConfig.js');
+const db = require('./database/dbHelpers');
 
 const server = express();
+const bcrypt = require('bcryptjs');
 
 server.use(express.json());
 server.use(cors());
@@ -21,5 +22,37 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 14);
+  db.insertUser(user)
+    .then(ids => {
+      res.status(201).json({id: ids[0]})
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+})
+
+server.post('/api/login', (req, res) => {
+  //check that username AND passwords match
+  const bodyUser = req.body;
+  db.findByUsername(bodyUser.username)
+    .then(users => {
+      if(users.length && bcrypt.compareSync(bodyUser.password, users[0].password)){
+        res.json({ info: "correct" })
+      } else {
+        res.status(404).json({ error: 'Invalid username or password' })
+      }
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+    ;
+})
+
+
+
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
