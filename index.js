71a1203f73d7fe33b781fe013cd34fa27868b291
Julie Jonak <juliejonak10@gmail.com>
2019-01-20 16:09:40 -0600

    For JSON Object lecture, added sessions.js to use instead of index.js to learn how to implement jwt library

--- index.js
+++ index.js
@@ -1,93 +1,94 @@
-const express = require('express');
-const cors = require('cors');
-const session = require('express-session');
+// const express = require('express');
+// const cors = require('cors');
+// const session = require('express-session');
+// const jwt = require('jsonwebtoken');
 
 
-const db = require('./database/dbHelpers');
+// const db = require('./database/dbHelpers');
 
-const server = express();
-const bcrypt = require('bcryptjs');
+// const server = express();
+// const bcrypt = require('bcryptjs');
 
-server.use(express.json());
-server.use(cors());
+// server.use(express.json());
+// server.use(cors());
 
-server.use(session({
-  name: 'notsession', // default is connect.sid
-  secret: 'nobody tosses a dwarf!', //this allows us to encrypt or unencrypt. we wouldn't want this to be hard coded or a string.
-  cookie: {
-    maxAge: 1 * 24 * 60 * 60 * 1000, //the age of our cookie
-  }, // this is 1 day in milliseconds
-  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
-  resave: false, //forces the session to be saved back to the session store, even if the session wasn't modified during the request
-  saveUninitialized: false, //do we want to make people give their consent for cookies? thing EU's GDPR laws
-}));
-
-
+// server.use(session({
+//   name: 'notsession', // default is connect.sid
+//   secret: 'nobody tosses a dwarf!', //this allows us to encrypt or unencrypt. we wouldn't want this to be hard coded or a string.
+//   cookie: {
+//     maxAge: 1 * 24 * 60 * 60 * 1000, //the age of our cookie
+//   }, // this is 1 day in milliseconds
+//   httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+//   resave: false, //forces the session to be saved back to the session store, even if the session wasn't modified during the request
+//   saveUninitialized: false, //do we want to make people give their consent for cookies? thing EU's GDPR laws
+// }));
 
 
-//custom middleware
-function protect(req, res, next){
-  if(req.session && req.session.userId){
-    next();
-  } else {
-    res.status(400).send('access denied')
-  }
-};
 
 
-server.get('/', (req, res) => {
-  res.send('Its Alive!');
-});
-
-// protect this route, only authenticated users should see it
-server.get('/api/users', protect, (req, res) => {
-  db.findUsers()
-    .then(users => {
-      res.json(users)
-    })
-    .catch(err => res.send(err));
-});
-
-server.post('/api/register', (req, res) => {
-  const user = req.body;
-  user.password = bcrypt.hashSync(user.password);
-  db.insertUser(user)
-    .then(ids => {
-      res.status(201).json({id: ids[0]})
-    })
-    .catch(err => {
-      res.status(500).send(err)
-    })
-})
-
-server.post('/api/login', (req, res) => {
-  const creds = req.body;
-  db.findByUsername(creds.username)
-    .then(users => {
-      //if username is valid and hashed passwords math
-      if(users.length && bcrypt.compareSync(creds.password, users[0].password)){
-        req.session.userId = users[0].id;
-        //redirect
-        res.json({ info: "correct" })
-      } else {
-        //redirect to the login screen with a new error message
-        res.status(404).json({ error: 'Invalid username or password' })
-      }
-    })
-    .catch(err => {
-      res.status(500).send(err)
-    })
-})
+// //custom middleware
+// function protect(req, res, next){
+//   if(req.session && req.session.userId){
+//     next();
+//   } else {
+//     res.status(400).send('access denied')
+//   }
+// };
 
 
-server.post('/api/logout', (req, res) => {
-  req.session.destroy(err => {
-    if(err){
-      res.status(500).send('failed to logout')
-    } else {
-      res.send('logout successful')
-    }
-  })
-})
+// server.get('/', (req, res) => {
+//   res.send('Its Alive!');
+// });
 
-server.listen(3300, () => console.log('\nrunning on port 3300\n'));
+// // protect this route, only authenticated users should see it
+// server.get('/api/users', protect, (req, res) => {
+//   db.findUsers()
+//     .then(users => {
+//       res.json(users)
+//     })
+//     .catch(err => res.send(err));
+// });
+
+// server.post('/api/register', (req, res) => {
+//   const user = req.body;
+//   user.password = bcrypt.hashSync(user.password);
+//   db.insertUser(user)
+//     .then(ids => {
+//       res.status(201).json({id: ids[0]})
+//     })
+//     .catch(err => {
+//       res.status(500).send(err)
+//     })
+// })
+
+// server.post('/api/login', (req, res) => {
+//   const creds = req.body;
+//   db.findByUsername(creds.username)
+//     .then(users => {
+//       //if username is valid and hashed passwords math
+//       if(users.length && bcrypt.compareSync(creds.password, users[0].password)){
+//         req.session.userId = users[0].id;
+//         //redirect
+//         res.json({ info: "correct" })
+//       } else {
+//         //redirect to the login screen with a new error message
+//         res.status(404).json({ error: 'Invalid username or password' })
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).send(err)
+//     })
+// }) 
+
+
+// server.post('/api/logout', (req, res) => {
+//   req.session.destroy(err => {
+//     if(err){
+//       res.status(500).send('failed to logout')
+//     } else {
+//       res.send('logout successful')
+//     }
+//   })
+// })
+
+// server.listen(3300, () => console.log('\nrunning on port 3300\n'));
---
+++ sessions.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const dbConfig = require('./knexfile');
+const server = express();
+const knex = require('knex');
+
+const db = knex(dbConfig.development);
+
+const bcrypt = require('bcryptjs');
+
+server.use(express.json());
+server.use(cors());
+
+
+
+//custom middleware
+const secret = 'seeeeeecret!';
+
+function generateToken(user){
+    const payload = {
+        username: user.username
+    };
+    const options = {
+        expiresIn: '1h',
+        jwtid: '12345', //JTI = jwt id, calculated based on the time you give the library
+    }
+    return jwt.sign(payload, secret, options);
+};
+
+
+function protected(req, res, next){
+    //use jwt instead of sessions
+    //read the token (string) from the Authorization header
+    const token = req.headers.authorization;
+    // Is the client sending a token?
+    if(token){
+        //verify the token
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if(err) {
+                //token is invalid
+                res.status(401).json({ message: "Invalid token." })
+            } else {
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({ message: "No token provided." })
+    }
+};
+
+server.post('/api/register', (req, res) => {
+    const creds = req.body;
+
+    const hash = bcrypt.hashSync(creds.password, 10);
+    creds.password = hash;
+
+    db('users')
+        .insert(creds)
+        .then(ids => {
+            const id = ids[0];
+            //find the user using the id
+            db('users').where({id}).first()
+                .then(user => {
+                    const token = generateToken(user);
+                    res.status(201).json({ id: user.id, token})
+                })
+                .catch(err => res.status(500).send(err))
+        })
+        .catch(err => res.status(500).send(err))
+})
+
+server.post('/api/login', (req, res) => {
+    const creds = req.body;
+
+    db('users')
+        .where({ username: creds.username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(creds.password, user.password)){
+                //generate a token
+                const token = generateToken(user);
+                res.status(200).json({ token})
+                //attach that token to the response
+                res.status(200).send(`Welcome ${user.username}`)
+            } else {
+                res.status(401).json({ message: 'You shall not pass!' })
+            }
+        })
+        .catch(err => res.status(500).send(err))
+});
+
+
+server.get('/api/users', protected, (req, res) => {
+      db('users')
+        .then(users => {
+        res.json(users)
+        })
+        .catch(err => res.send(err));
+});
+
+server.get('/', (req, res) => {
+    res.send('It is aliiiiiive!')
+});
+
+
+//SERVER
+
+server.listen(3600, () => console.log('\nrunning on port 3600\n'));
